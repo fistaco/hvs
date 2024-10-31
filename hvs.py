080808c01428052c64513921e7ec267f556921c3
fistaco <fiske_schijlen@hotmail.com>
2024-10-31 13:48:03 +0100

    Add review of incorrect answers. Still has repetition for now.

--- hvs.py
+++ hvs.py
@@ -3,6 +3,23 @@ import json
 import os
 import random
 from fuzzywuzzy import fuzz
+import argparse
+
+
+INCORRECT_ENTRIES_FILEPATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), "incorrect_entries.json")
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(dest='subcommand')
+
+    quiz_parser = subparsers.add_parser('quiz')
+    quiz_parser.set_defaults(subcommand='quiz')
+
+    review_parser = subparsers.add_parser('review')
+    review_parser.set_defaults(subcommand='review')
+
+    return parser.parse_args()
 
 
 GREEN = "\033[92m"
@@ -10,23 +27,33 @@ RED = "\033[91m"
 RESET = "\033[0m"
 
 
-if __name__ == "__main__":
-    hsk_vocab_filepath = os.path.abspath(os.path.dirname(__file__)) + os.sep + "complete-hsk-vocabulary/by-level/new/1.json"
-    with open(hsk_vocab_filepath, 'r', encoding='utf-8') as file:
-        vocab_items = json.load(file)
+def save_vocab_entries(filepath: str, entries: dict):
+    """
+    Save the vocabulary entries to a JSON file.
+    :param filepath: The path of the file to save the entries to.
+    :param entries: A dictionary containing the vocabulary entries.
+    """
+    with open(filepath, 'w', encoding='utf-8') as file:
+        json.dump(entries, file, ensure_ascii=False)
 
+
+def quiz(vocab_entries):
+    """
+    Quiz the user on the given vocabulary entries.
+    """
     num_correct = 0
     num_total = 0
+    incorrect_entries = []
 
     while True:
-        vocab_entry = random.choice(vocab_items)
+        vocab_entry = random.choice(vocab_entries)
         forms_entry = random.choice(vocab_entry["forms"])
 
         pinyin = forms_entry["transcriptions"]["pinyin"]
         simplified = vocab_entry["simplified"]
 
         meanings = forms_entry["meanings"]
-        if len(meanings) == 0 and "variant" in meanings[0]:
+        if len(meanings) == 1 and "variant" in meanings[0]:
             continue
 
         try:
@@ -34,6 +61,9 @@ if __name__ == "__main__":
         except KeyboardInterrupt:
             correct_ratio = num_correct / num_total
             print(f"Final score: {num_correct}/{num_total}  ({correct_ratio})")
+
+            print(f"Saving incorrect entries to {INCORRECT_ENTRIES_FILEPATH}")
+            save_vocab_entries(INCORRECT_ENTRIES_FILEPATH, incorrect_entries)
 
             print("Exiting.")
             exit(0)
@@ -55,5 +85,26 @@ if __name__ == "__main__":
             num_correct += 1
         else:
             print(f"{RED}False{RESET} (similarity score = {max_score})")
+            incorrect_entries.append(vocab_entry)
 
         print("All translations: ", processed_meanings, "\n")
+
+
+
+if __name__ == "__main__":
+    args = parse_args()
+
+    dir_name = os.path.abspath(os.path.dirname(__file__))
+
+    vocab_entries_filepath = None
+    if args.subcommand == "quiz":
+        vocab_entries_filepath = dir_name + os.sep + "complete-hsk-vocabulary/by-level/new/1.json"
+    elif args.subcommand == "review":
+        vocab_entries_filepath = INCORRECT_ENTRIES_FILEPATH
+    else:
+        print("Invalid subcommand")
+        exit(1)
+
+    with open(vocab_entries_filepath, 'r', encoding='utf-8') as file:
+        vocab_entries = json.load(file)
+    quiz(vocab_entries)
